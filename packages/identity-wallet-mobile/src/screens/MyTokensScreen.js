b383d2e0d31c801cff0296b7f7344cc84b8b204d
Maycon Mello <maycon.mellos@gmail.com>
2019-08-29 11:30:01 -0300

    Fix for eslint issue on MyTokenScreen

--- packages/identity-wallet-mobile/src/screens/MyTokensScreen.js
+++ packages/identity-wallet-mobile/src/screens/MyTokensScreen.js
@@ -1,9 +1,17 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default function MyTokensScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={styles.container}>
       <Text>My Tokens (Dummy)</Text>
     </View>
   );
