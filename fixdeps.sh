1f6795ee1edabb9f07472f042ee6180ca3fb1700
Maycon Mello <maycon.mellos@gmail.com>
2019-08-29 09:51:55 -0300

    Workaround for issue with metro build

--- fixdeps.sh
+++ fixdeps.sh
@@ -1,5 +1,3 @@
 cp -rf ./node_modules/@react-native-community/* ./packages/identity-wallet-mobile/node_modules/@react-native-community
 cp -rf ./node_modules/react-native-reanimated ./packages/identity-wallet-mobile/node_modules/react-native-reanimated
-
-
-
+sed -i".bkp" -e "s/ existingModule \&\&/false \&\&/g" ./node_modules/jest-haste-map/build/index.js
