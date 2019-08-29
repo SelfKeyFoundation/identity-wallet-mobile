dbc5652ca6e684da25eb5a2f7fddfa55ee880510
Maycon Mello <maycon.mellos@gmail.com>
2019-08-29 11:12:50 -0300

    Use selfkey eslint-config on mobile-ui

--- packages/mobile-ui/.eslintrc.js
+++ packages/mobile-ui/.eslintrc.js
@@ -1,8 +1,4 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
-  env: {
-    jest: true,
-    "react-native/react-native": true
-  }
+  extends: '@selfkey/eslint-config',
 };
