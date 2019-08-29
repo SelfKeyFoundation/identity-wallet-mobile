edfc8b0719d8fbc6ff59bde77f072e96662cb3a9
Maycon Mello <maycon.mellos@gmail.com>
2019-08-29 09:52:26 -0300

    Create cleanCache script

---
+++ cleanCache.sh
@@ -0,0 +1,9 @@
+watchman watch-del-all
+rm -rf $TMPDIR/metro-bundler-cache-*
+rm -rf $TMPDIR/react-native-packager-cache-*
+rm -rf $TMPDIR/haste-map-react-native-packager-*
+rm -rf node_modules/st
+rm -rf packages/*/node_modules
+
+
+yarn start --reset-cache
