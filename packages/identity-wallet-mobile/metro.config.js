fa2e63a450b57739550011ae972ff363b7bb9865
Maycon Mello <maycon.mellos@gmail.com>
2019-08-29 09:51:37 -0300

    Update metro configs

--- packages/identity-wallet-mobile/metro.config.js
+++ packages/identity-wallet-mobile/metro.config.js
@@ -6,6 +6,7 @@
  * @format
  */
 const path = require('path');
+const blacklist = require('metro-config/src/defaults/blacklist');
 
 require('./link-workspaces.js');
 
@@ -15,7 +16,15 @@ module.exports = {
     path.resolve(__dirname, "../blockchain"),
     path.resolve(__dirname, "../identity-wallet-core"),
     path.resolve(__dirname, "../../node_modules"),
+    path.resolve(__dirname, "../mobile-ui/node_modules"),
+    // path.resolve(__dirname, "./node_modules/styled-components/native"),
   ],
+  resolver: {
+    // blacklistRE: blacklist([
+      // /mobile-ui\/node_modules\/react-native\/.*/,
+      // /mobile-ui\/.*\/node_modules\/react-native\/.*/
+    // ])
+  },
   transformer: {
     getTransformOptions: async () => ({
       transform: {
