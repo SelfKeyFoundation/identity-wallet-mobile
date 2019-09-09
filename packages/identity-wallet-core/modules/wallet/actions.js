bcabb1bb4cde173647dca3f044d6a99cbbf320c8
Maycon Mello <maycon.mellos@gmail.com>
2019-09-09 18:56:35 -0300

    Refactor on actions and types

--- packages/identity-wallet-core/modules/wallet/actions.js
+++ packages/identity-wallet-core/modules/wallet/actions.js
@@ -1,10 +1,14 @@
 // @flow
-import { LOAD_WALLET } from './types';
+import {
+  LOAD_WALLET,
+} from './types';
 
+// action types
 export type LoadWalletAction = {
   type: LOAD_WALLET,
 };
 
+// actions
 export function loadWallet(): LoadWalletAction {
   return {
     type: LOAD_WALLET,
--- packages/identity-wallet-core/modules/wallet/types.js
+++ packages/identity-wallet-core/modules/wallet/types.js
@@ -1,3 +1,2 @@
 // @flow
-
 export const LOAD_WALLET = 'wallet/load';
