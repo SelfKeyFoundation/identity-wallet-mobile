46f68e2f4a409d2b1559606ee0cbc07947d94a08
Maycon Mello <maycon.mellos@gmail.com>
2019-09-10 12:03:31 -0300

    Enable redux-thunk middleware

--- packages/identity-wallet-mobile/src/setup-redux.js
+++ packages/identity-wallet-mobile/src/setup-redux.js
@@ -1,4 +1,5 @@
 import '@selfkey/wallet-core/redux/middlewares/register-redux-logger';
+import '@selfkey/wallet-core/redux/middlewares/register-redux-thunk';
+// We can enable redux saga when needed
 // import '@selfkey/wallet-core/redux/middlewares/register-redux-saga';
-// import '@selfkey/wallet-core/redux/middlewares/register-redux-thunk';
 import '@selfkey/wallet-core/register-core-modules';
