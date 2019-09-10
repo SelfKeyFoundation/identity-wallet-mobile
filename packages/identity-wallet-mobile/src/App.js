9aed5a2b1c5b3a9dbe46367a3a86b8eaa193913c
Maycon Mello <maycon.mellos@gmail.com>
2019-09-10 11:03:25 -0300

    Integration mobile wallet with redux core

--- packages/identity-wallet-mobile/src/App.js
+++ packages/identity-wallet-mobile/src/App.js
@@ -1,14 +1,38 @@
 import React from 'react';
-import Navigation from './navigation';
+import { View, Text, Button } from 'react-native';
 import './setup-redux';
-import { createStoreProvider } from '@selfkey/wallet-core/redux';
+import Navigation from './navigation';
+import { createStoreProvider, connect } from '@selfkey/wallet-core/redux';
+import { loadWallet } from '@selfkey/wallet-core/modules/wallet/actions';
 
 const Provider = createStoreProvider();
 
-export default function App() {
+const mapStateToProps = (state) => {
+  return ({
+    isLoading: state.wallet.isLoading,
+  });
+};
+
+const mapActionsToProps = (dispatch) => ({
+  setLoading: () => dispatch(loadWallet()),
+});
+
+const AppStatus = connect(mapStateToProps, mapActionsToProps)((props) => {
+  return (
+    <View>
+      <Text>Is app { props.isLoading ? 'loading' : 'ready' }</Text>
+      <Button onPress={props.setLoading} title="Set loading"/>
+    </View>
+  );
+});
+
+function App(props) {
   return (
     <Provider>
+      <AppStatus />
       <Navigation />
     </Provider>
   );
 }
+
+export default App;
--- packages/identity-wallet-mobile/src/setup-redux.js
+++ packages/identity-wallet-mobile/src/setup-redux.js
@@ -1,4 +1,4 @@
 import '@selfkey/wallet-core/redux/middlewares/register-redux-logger';
-import '@selfkey/wallet-core/redux/middlewares/register-redux-saga';
-import '@selfkey/wallet-core/redux/middlewares/register-redux-thunk';
+// import '@selfkey/wallet-core/redux/middlewares/register-redux-saga';
+// import '@selfkey/wallet-core/redux/middlewares/register-redux-thunk';
 import '@selfkey/wallet-core/register-core-modules';
