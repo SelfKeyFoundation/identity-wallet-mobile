25cf4d3aab92a9785b772d4d9cde4a5eb2d43e1f
Maycon Mello <maycon.mellos@gmail.com>
2019-09-18 16:31:05 -0300

    Implement BaseModel using realm

--- packages/wallet-core/db/models/BaseModel.js
+++ packages/wallet-core/db/models/BaseModel.js
@@ -12,7 +12,52 @@ export class BaseModel {
     });
   }
 
-  getAll() {
+  removeById(id) {
+    return this.realm.write(() => {
+      const item = this.findById(id);
+      this.realm.delete(item);
+    });
+  }
+
+  write(func) {
+    return this.realm.write(func);
+  }
+
+  removeAll() {
+    return this.write(() => {
+      let allItems = this.realm.objects(this.schema.name);
+      this.realm.delete(allItems);
+    });
+  }
+
+  findAll() {
     return this.realm.objects(this.schema.name);
+  }
+
+  findById(id) {
+    const items = this.findAll().filtered(`id = ${id}`);
+    return items[0];
+  }
+
+  updateById(id, data) {
+    return this.realm.write(() => {
+      return this.realm.create(this.schema.name, {
+        ...data,
+        id,
+      }, true);
+    });
+  }
+
+  /**
+   * Query example: color = "tan" AND name BEGINSWITH "B"
+   * @param {string} query query string
+   *
+   * The query language supported by Realm is inspired by Apple’s NSPredicate
+   * https://realm.io/docs/javascript/latest#filtering
+   * https://developer.apple.com/documentation/foundation/nspredicate
+   *
+   */
+  find(query, ...args) {
+    return this.findAll().filtered(query, ...args);
   }
 }
