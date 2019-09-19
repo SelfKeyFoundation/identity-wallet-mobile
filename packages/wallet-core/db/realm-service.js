8f12b7401c1d00e5f837913390e821db8090dd76
Maycon Mello <maycon.mellos@gmail.com>
2019-09-18 23:24:22 -0300

    Create migration system for Realm

--- packages/wallet-core/db/realm-service.js
+++ packages/wallet-core/db/realm-service.js
@@ -1,6 +1,8 @@
 let Realm;
 const models = [];
 let realmInstance;
+import migrations from './migrations';
+import schemas from './schemas';
 
 /**
  * Mobile wallet will inject a mobile version
@@ -30,7 +32,25 @@ export function setRealmInstance(instance) {
   realmInstance = instance;
 }
 
-export function initRealm(opts) {
+export function initRealm(opts = {}) {
+  // The first schema to update to is the current schema version
+  // since the first schema in our array is at nextSchemaIndex:
+  let nextSchemaIndex = Realm.schemaVersion(Realm.defaultPath);
+
+  // If Realm.schemaVersion() returned -1, it means this is a new Realm file
+  // so no migration is needed.
+  if (nextSchemaIndex !== -1) {
+    while (nextSchemaIndex < schemas.length) {
+      const { schema, schemaVersion } = schemas[nextSchemaIndex++];
+      const migratedRealm = new Realm({
+        schema,
+        schemaVersion,
+        migration: migrations[schemaVersion],
+      });
+      migratedRealm.close();
+    }
+  }
+
   return Realm.open({
     schema: models.map(m => m.schema),
     ...opts,
