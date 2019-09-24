db94bcfa86f25d7c1d73af694cee571c70a60374
Maycon Mello <maycon.mellos@gmail.com>
2019-09-23 23:40:22 -0300

    Remove unnecessary file

--- clean-modules.sh
+++
@@ -1,6 +0,0 @@
-rm -rf node_modules
-rm -rf packages/identity-wallet-mobile/node_modules
-rm -rf packages/mobile-ui/node_modules
-rm -rf packages/wallet-core/node_modules
-rm -rf packages/mobile-ui-storybook/node_modules
-
