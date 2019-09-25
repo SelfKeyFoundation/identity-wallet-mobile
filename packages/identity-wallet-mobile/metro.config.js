23ec822a6fb89401e06c09866343da3cbe5bdd09
Maycon Mello <maycon.mellos@gmail.com>
2019-09-25 08:55:05 -0300

    Configure support for svg files

--- packages/identity-wallet-mobile/metro.config.js
+++ packages/identity-wallet-mobile/metro.config.js
@@ -10,27 +10,58 @@ const blacklist = require('metro-config/src/defaults/blacklist');
 
 require('./link-workspaces.js');
 
-module.exports = {
-  watchFolders: [
-    path.resolve(__dirname, "../mobile-ui"),
-    path.resolve(__dirname, "../blockchain"),
-    path.resolve(__dirname, "../wallet-core"),
-    path.resolve(__dirname, "../../node_modules"),
-    path.resolve(__dirname, "../mobile-ui/node_modules"),
-    // path.resolve(__dirname, "./node_modules/styled-components/native"),
-  ],
-  resolver: {
-    // blacklistRE: blacklist([
-      // /mobile-ui\/node_modules\/react-native\/.*/,
-      // /mobile-ui\/.*\/node_modules\/react-native\/.*/
-    // ])
-  },
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: false,
-      },
-    }),
-  }
-};
+// module.exports = {
+//   watchFolders: [
+//     path.resolve(__dirname, "../mobile-ui"),
+//     path.resolve(__dirname, "../blockchain"),
+//     path.resolve(__dirname, "../wallet-core"),
+//     path.resolve(__dirname, "../../node_modules"),
+//     path.resolve(__dirname, "../mobile-ui/node_modules"),
+//     // path.resolve(__dirname, "./node_modules/styled-components/native"),
+//   ],
+//   resolver: {
+//     // blacklistRE: blacklist([
+//       // /mobile-ui\/node_modules\/react-native\/.*/,
+//       // /mobile-ui\/.*\/node_modules\/react-native\/.*/
+//     // ])
+//   },
+//   transformer: {
+//     getTransformOptions: async () => ({
+//       transform: {
+//         experimentalImportSupport: false,
+//         inlineRequires: false,
+//       },
+//     }),
+//   }
+// };
+
+const { getDefaultConfig } = require('metro-config');
+
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts, assetExts },
+  } = await getDefaultConfig();
+  return {
+    watchFolders: [
+      path.resolve(__dirname, "../mobile-ui"),
+      path.resolve(__dirname, "../blockchain"),
+      path.resolve(__dirname, "../wallet-core"),
+      path.resolve(__dirname, "../../node_modules"),
+      path.resolve(__dirname, "../mobile-ui/node_modules"),
+      // path.resolve(__dirname, "./node_modules/styled-components/native"),
+    ],
+    transformer: {
+      babelTransformerPath: require.resolve('react-native-svg-transformer'),
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: false,
+        },
+      }),
+    },
+    resolver: {
+      assetExts: assetExts.filter(ext => ext !== 'svg'),
+      sourceExts: [...sourceExts, 'svg'],
+    },
+  };
+})();
