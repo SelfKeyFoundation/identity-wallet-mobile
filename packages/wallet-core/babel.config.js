47ef7f6bdbfbba199678a2d993439c9ae471437b
Maycon Mello <maycon.mellos@gmail.com>
2019-09-25 08:58:00 -0300

    Cleanup in babel.config.js

--- packages/wallet-core/babel.config.js
+++ packages/wallet-core/babel.config.js
@@ -1,23 +1,15 @@
 module.exports = {
-  "presets": [
-    "@babel/preset-env",
-    '@babel/preset-react'
+  'presets': [
+    '@babel/preset-env',
+    '@babel/preset-react',
   ],
-  "plugins": [
-    // [
-    //   require.resolve("babel-plugin-module-resolver"),
-    //   {
-    //     "react": [
-    //       "../../node_modules/react"
-    //     ]
-    //   }
-    // ],
-    "@babel/plugin-transform-flow-strip-types",
-    "@babel/plugin-proposal-class-properties",
-    ["@babel/plugin-transform-runtime",
+  'plugins': [
+    '@babel/plugin-transform-flow-strip-types',
+    '@babel/plugin-proposal-class-properties',
+    ['@babel/plugin-transform-runtime',
       {
-        "regenerator": true
-      }
-    ]
-  ]
-}
+        'regenerator': true,
+      },
+    ],
+  ],
+};
