ad86cce571eddbb03666505c280f5a5e4d0eae7b
Maycon Mello <maycon.mellos@gmail.com>
2019-09-29 22:58:54 -0300

    Loading screen

--- packages/identity-wallet-mobile/src/screens/loading-screen/LoadingScreen.js
+++ packages/identity-wallet-mobile/src/screens/loading-screen/LoadingScreen.js
@@ -1,22 +1,29 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import { SkBackground, SkLogo, Paragraph } from '@selfkey/mobile-ui/lib';
+import styled from 'styled-components/native';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
+const Title = styled.Text`
+  color: #0ABBD0;
+  font-size: 18px;
+  font-family: Lato-Regular;
+  margin-top: 20;
+`;
+
+const Container = styled.View`
+  flex: 1;
+  justify-content: flex-start;
+  align-items: center;
+  top: 19%;
+`;
 
 export default function LoadingScreen() {
   return (
     <SkBackground>
-      <View style={styles.container}>
+      <Container>
         <SkLogo />
-        <Paragraph>IDENTITY WALLET</Paragraph>
-      </View>
+        <Title>IDENTITY WALLET</Title>
+      </Container>
     </SkBackground>
   );
 }
