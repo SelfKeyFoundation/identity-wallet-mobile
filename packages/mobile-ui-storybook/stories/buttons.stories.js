b68f276ece3310b7b35b2c0da18e064a4f18b90b
Maycon Mello <maycon.mellos@gmail.com>
2019-10-21 15:48:37 -0300

    Create story example for button

---
+++ packages/mobile-ui-storybook/stories/buttons.stories.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import { storiesOf } from '@storybook/react-native';
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
+import { PrimaryButton } from '@selfkey/mobile-ui/lib/buttons/PrimaryButton';
+
+storiesOf('Buttons', module)
+  .addDecorator(getStory => <View>{getStory()}</View>)
+  .add('Primary', () => (
+    <PrimaryButton onPress={action('buttons-press')}>
+      Primary Button
+    </PrimaryButton>
+  ))
