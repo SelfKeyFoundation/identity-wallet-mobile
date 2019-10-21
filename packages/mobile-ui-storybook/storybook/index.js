276707363504befaee837fe7a83f069cb02ec426
Maycon Mello <maycon.mellos@gmail.com>
2019-10-21 15:45:42 -0300

    Refactor on stories

--- packages/mobile-ui-storybook/storybook/index.js
+++ packages/mobile-ui-storybook/storybook/index.js
@@ -1,13 +1,17 @@
+import React from 'react';
 import { AppRegistry } from 'react-native';
-import { getStorybookUI, configure } from '@storybook/react-native';
+import { getStorybookUI, configure, addDecorator } from '@storybook/react-native';
+import { Provider as PaperProvider } from 'react-native-paper';
 
 import './rn-addons';
 
-// import stories
-configure(() => {
-  require('./stories');
-}, module);
+addDecorator(storyFn => (
+  <PaperProvider>
+    { storyFn() }
+  </PaperProvider>
+));
 
+configure(() => require('../stories'), module);
 
 // Refer to https://github.com/storybookjs/storybook/tree/master/app/react-native#start-command-parameters
 // To find allowed options for getStorybookUI
