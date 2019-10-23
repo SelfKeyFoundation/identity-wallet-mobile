7c64034f66910d11c7a0ac1f3e2665fc81ee8e7c
Maycon Mello <maycon.mellos@gmail.com>
2019-10-23 07:58:01 -0300

    Setup colors

---
+++ packages/mobile-ui/lib/theme.js
@@ -0,0 +1,16 @@
+import { colors } from './colors';
+export const Theme = {
+  colors: {
+    uiPrimary: colors.darkTurquoise,
+    uiPrimaryTint: colors.brightTurquoise,
+    uiTypography: colors.spindle,
+    uiWhite: colors.white,
+    uiGrey: colors.slateGrey,
+    uiBaseLight: colors.cello,
+    uiBaseDark: colors.blueCharcoal,
+    uiBase: colors.midnight,
+    uiSuccess: colors.mountainMeadow,
+    uiWarning: colors.flamenco,
+    uiError: colors.bittersweet,
+  }
+}
