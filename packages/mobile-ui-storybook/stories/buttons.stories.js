ad1d3fb1eb6273f4db24f68d918af537ffe26745
Maycon Mello <maycon.mellos@gmail.com>
2019-10-24 13:47:13 -0300

    Create stories for typography

--- packages/mobile-ui-storybook/stories/buttons.stories.js
+++ packages/mobile-ui-storybook/stories/buttons.stories.js
@@ -3,7 +3,7 @@ import { Text, View } from 'react-native';
 import { storiesOf } from '@storybook/react-native';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
-import { PrimaryButton } from '@selfkey/mobile-ui/lib/buttons/PrimaryButton';
+import { PrimaryButton } from '@selfkey/mobile-ui';
 
 storiesOf('Buttons', module)
   .addDecorator(getStory => (
