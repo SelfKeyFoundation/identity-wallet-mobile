a3c79288d2858805729f5f17e6e021d477a1db0f
Maycon Mello <maycon.mellos@gmail.com>
2019-11-11 15:21:20 -0300

    Feat/rn nodeify (#87)
    * Merge master into feat/buttons
    
    * Stories for buttons
    
    * Stories for buttons
    
    * Remove test code
    
    * Implement text input
    
    * Inputs stories
    
    * Implement text input
    
    * Navigation
    
    * Icons
    
    * Remove .DS_Store files and update .gitignore
    
    * Merge master into feat/icons
    
    * Alerts
    
    * App icon
    
    * Fix issue for android
    
    * Apply rn-nodeify
    
    * fix(gitignore): add history and package-lock

--- packages/identity-wallet-mobile/index.js
+++ packages/identity-wallet-mobile/index.js
@@ -5,5 +5,6 @@
 import {AppRegistry} from 'react-native';
 import {Root} from './src/Root';
 import {name as appName} from './app.json';
+import './shim.js';
 
 AppRegistry.registerComponent(appName, () => Root);
