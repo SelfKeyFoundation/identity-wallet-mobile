ab017e4201823f6a91404844606acb11387092ba
Maycon Mello <maycon.mellos@gmail.com>
2019-11-13 14:50:45 -0300

    Create password redux

---
+++ packages/wallet-core/modules/create-wallet/types.js
@@ -0,0 +1,5 @@
+export const walletTypes = {
+  SET_PASSWORD : 'createWallet/setPassword',
+};
+
+export default walletTypes;
--- packages/wallet-core/modules/index.js
+++ packages/wallet-core/modules/index.js
@@ -1,7 +1,9 @@
 import wallet from './wallet';
+import createWallet from './create-wallet';
 import app from './app';
 
 export default {
   wallet,
   app,
+  createWallet,
 };
