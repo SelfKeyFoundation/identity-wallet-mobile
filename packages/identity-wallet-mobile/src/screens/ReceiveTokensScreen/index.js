1502cc2ac87578c87eaca8a2db5a6ad1a4f04f19
Maycon Mello <maycon.mellos@gmail.com>
2019-12-18 11:09:33 -0300

    Share token (#136)

--- packages/identity-wallet-mobile/src/screens/ReceiveTokensScreen/index.js
+++ packages/identity-wallet-mobile/src/screens/ReceiveTokensScreen/index.js
@@ -1,6 +1,7 @@
 import React, { useCallback, useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Dashboard } from '../DashboardScreen/Dashboard';
+import { Clipboard, Share } from 'react-native';
 import { ReceiveTokens } from '../../components';
 import modules from '@selfkey/wallet-core/modules';
 import { navigate, Routes } from '@selfkey/wallet-core/navigation';
@@ -11,11 +12,22 @@ function ReceiveTokensContainer(props) {
   const tokenSymbol = props.navigation.getParam('tokenSymbol', 'ETH');
   const wallet = useSelector(selectors.getWallet);
   const [visible, setVisible] = useState(true);
+  const { address } = wallet;
 
   const handleClose = useCallback(() => {
     setVisible(false);
     navigate(Routes.APP_DASHBOARD);
   }, [setVisible]);
+
+  const handleCopy = useCallback(() => {
+    Clipboard.setString(address);
+  }, [address]);
+
+  const handleShare = useCallback(() => {
+    Share.share({
+      message: address,
+    });
+  }, [address]);
 
   useEffect(() => {
     setVisible(true);
@@ -26,9 +38,11 @@ function ReceiveTokensContainer(props) {
       <Dashboard />
       <ReceiveTokens
         tokenSymbol={tokenSymbol}
-        tokenAddress={wallet.address}
+        tokenAddress={address}
         onClose={handleClose}
         visible={visible}
+        onCopy={handleCopy}
+        onShare={handleShare}
       />
     </React.Fragment>
   );
