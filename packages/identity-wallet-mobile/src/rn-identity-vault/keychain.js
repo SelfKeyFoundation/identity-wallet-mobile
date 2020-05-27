bbb0259eca51471e9014d8e3ff301c9d054e255a
Maycon Mello <maycon.mellos@gmail.com>
2020-05-27 07:56:48 -0300

    feat(biometrics): setup

--- packages/identity-wallet-mobile/src/rn-identity-vault/keychain.js
+++ packages/identity-wallet-mobile/src/rn-identity-vault/keychain.js
@@ -43,3 +43,41 @@ export async function getItem(id: string): any {
 
   return data;
 }
+
+export const BiometryTipes = {
+  TOUCH_ID: 'TouchID',
+  FACE_ID: 'FaceID',
+  FINGERPRINT: 'Fingerprint',
+};
+
+/**
+ * Values:
+ * 
+ */
+export function getSupportedBiometryType() {
+  return Keychain.getSupportedBiometryType();
+}
+
+getItem('test').then((data) => {
+  // console.log(data);
+  debugger;
+}).catch(err => {
+  // console.log(err);
+  debugger;
+})
+
+// Keychain.setGenericPassword('test', 'test2', {
+//   accessControl: Keychain.ACCESS_CONTROL.BIOMETRY_ANY,
+//   service: 'testing'
+// }).then(result => {
+//   // console.log(result);
+//   Keychain.getGenericPassword({
+//     service: 'testing',
+//     // accessControl: Keychain.ACCESS_CONTROL.BIOMETRY_ANY,
+//     // authenticationType: Keychain.AUTHENTICATION_TYPE.BIOMETRICS
+//     authenticationPrompt: 'Unlock your selfkey wallet',
+//   }).then(data => {
+//     console.log(data);
+//   });  
+// });
+// https://github.com/SelfKeyFoundation/identity-wallet-mobile/commit/95ac1e01db156a2854195b5b51b77eb89f755c61
