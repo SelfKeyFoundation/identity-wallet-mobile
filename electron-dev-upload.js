b26ec78c1a018eb5e4a0089e292f7d6db8e6c9c3
mzmsk <mzmcode2@gmail.com>
2022-12-21 11:20:57 -0300

    feat: updates

--- electron-dev-upload.js
+++ electron-dev-upload.js
@@ -11,8 +11,12 @@ const commitDate = new Date(getCommandOutput('git show -s --format=%ci'));
 const formattedDate = moment(commitDate).format('YYYY-MM-DD_HHMMss')
 const bucketFolder = `gs://sk-builds/unified-wallet/${formattedDate}/`;
 
-console.log(`Uploading artifacts to: ` bucketFolder);
+console.log(`Uploading artifacts to: ${bucketFolder}`);
+
+glob.sync('./out/**/*').forEach(f => console.log(f));
 
 glob.sync('./out/**/*.zip').forEach(file => {
+  console.log(`File: ${file}`);
+
   getCommandOutput(`gsutil cp ${file} ${bucketFolder}`)
 });
