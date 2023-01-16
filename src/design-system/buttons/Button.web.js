632ea19a5a1019bc751405098b7c76e16d4a88f0
mzmsk <mzmcode2@gmail.com>
2023-01-16 14:24:28 -0300

    fix: buttons

--- src/design-system/buttons/Button.web.js
+++ src/design-system/buttons/Button.web.js
@@ -5,11 +5,49 @@ import { View, ActivityIndicator } from 'react-native';
 import { PaperThemeContext, ThemeContext } from '../mobile-ui-provider';
 import LinearGradient from 'react-native-linear-gradient';
 import { Button as NButton } from 'native-base';
+import { Theme } from '../theme';
 
 export const Button = (props: ButtonProps) => {
-  return <NButton {...props} _text={{
+
+  const style = {
+    fontFamily: Theme.fonts.bold,
+    fontSize: 14,
+    textTransform: 'uppercase',
+    color: Theme.colors.baseDark,
+    letterSpacing: 0,
+    borderRadius: '50px',
+  };
+
+  const textStyle = {
     color: "#111111"
-  }} style={{
-    background: 'linear-gradient(107.37deg, #00E0FF 0%, #2DA1F8 100%)'
-  }}/>
+  }
+
+  if (props.type === 'shell-secondary') {
+    style.borderColor = '#93B0C1';
+    style.borderWidth = 2;
+    textStyle.color = '#C5DCE9';
+    style.background = 'none';
+  }
+
+  if (props.type === 'shell-primary') {
+    style.background = 'none';
+    style.borderColor = Theme.colors.primary;
+    style.borderWidth = 2;
+    textStyle.color = Theme.colors.primary;
+    textStyle.top = -2;
+    style.height = 40;
+  }
+
+  if (props.type === 'link') {
+    style.borderWidth = 0;
+    textStyle.color = Theme.colors.link;
+    textStyle.fontWeight = 'normal';
+    style.background = 'none';
+  }
+
+  if (!props.type || props.type === 'full-primary' || props.type === 'primary') {
+    style.background = 'linear-gradient(107.37deg, #00E0FF 0%, #2DA1F8 100%)';
+  }
+  
+  return <NButton {...props} _text={textStyle} style={style}/>
 };
