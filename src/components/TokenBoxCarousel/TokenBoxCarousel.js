d5a4a351d897a4d4c7efc3e711572776e56ce86e
mzmsk <mzmcode2@gmail.com>
2023-01-19 22:36:16 -0300

    fix: scrolling issues

--- src/components/TokenBoxCarousel/TokenBoxCarousel.js
+++ src/components/TokenBoxCarousel/TokenBoxCarousel.js
@@ -59,6 +59,7 @@ function useWindowSize() {
 
 export function TokenBoxCarousel(props: TokenBoxCarouselProps) {
   const ref = useRef();
+  const scrolledRef = useRef();
   const pressTimeoutRef = useRef();
   const { width: viewportWidth } = useWindowSize();
 
@@ -66,18 +67,26 @@ export function TokenBoxCarousel(props: TokenBoxCarouselProps) {
     const prevIndex = ref.current?.getCurrentIndex();
     clearTimeout(pressTimeoutRef.current);
 
+    console.log(ref.current);
+
     pressTimeoutRef.current = setTimeout(() => {
-      if (ref.current?.getCurrentIndex() === prevIndex) {
+      if (ref.current?.getCurrentIndex() === prevIndex && !scrolledRef.current) {
         pressHandler();
       }
-    }, 300);
-  }, [ref, pressTimeoutRef]);
+    }, 500);
+  }, [ref, pressTimeoutRef, scrolledRef]);
 
   return (
     <View style={{ borderColor: 'black', borderStyle: 'solid', borderWidth: 0, height: 230 }}>
       <Carousel
         ref={ref}
         width={viewportWidth}
+        onScrollBegin={() => {
+          scrolledRef.current = true;
+        }}
+        onScrollEnd={() => {
+          scrolledRef.current = false;
+        }}
         height={230}
         data={props.items}
         renderItem={(props) => renderItem({ ...props, handlePress: handlePressAction})}
